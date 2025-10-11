38a61c306773e87c87ef6cf4ba04e029adbc02cf
vb003 <s30644@pjwstk.edu.pl>
2025-10-11 15:15:36 +0200

    feat(hero-movement): add jumping and coyote time
    player can now jump from the ground or shortly after falling off a platform

--- Assets/Scripts/Player/BasicPlayerMovement.cs
+++ Assets/Scripts/Player/BasicPlayerMovement.cs
@@ -7,6 +7,13 @@ public class BasicPlayerMovement : MonoBehaviour
     private float _xInput;
     [SerializeField] private float speed = 5;
 
+    private bool _performJump;
+    private bool _isGrounded;
+    [SerializeField] private float jumpForce = 5;
+
+    private float _coyoteTime = 0.2f;
+    private float _coyoteTimeCounter;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -15,10 +22,46 @@ public class BasicPlayerMovement : MonoBehaviour
     private void Update()
     {
         _xInput = Input.GetAxis("Horizontal");
+
+        if (_isGrounded)
+        {
+            _coyoteTimeCounter = _coyoteTime;
+        }
+        else
+        {
+            _coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && _coyoteTimeCounter > 0)
+        {
+            _performJump = true;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            _coyoteTimeCounter = 0;
+        }
     }
 
     private void FixedUpdate()
     {
         _rb.linearVelocity = new Vector2(_xInput * speed, _rb.linearVelocity.y);
+
+        if (_performJump)
+        {
+            _performJump = false;
+            _isGrounded = false;
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _isGrounded = false;
     }
 }
