3d63cb18fad493b9694f565a27e4fdf3f7a1b18d
vb003 <s30644@pjwstk.edu.pl>
2025-10-12 20:28:42 +0200

    fix(hero-movement): improve hero ground detection
    - the player is no longer considered grounded when touching walls
    - rename SampleScene to VanessaExperimentalScene for clarity

--- Assets/Scripts/Player/BasicPlayerMovement.cs
+++ Assets/Scripts/Player/BasicPlayerMovement.cs
@@ -1,67 +1,74 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class BasicPlayerMovement : MonoBehaviour
+namespace Player
 {
-    private Rigidbody2D _rb;
-    private float _xInput;
-    [SerializeField] private float speed = 5;
-
-    private bool _performJump;
-    private bool _isGrounded;
-    [SerializeField] private float jumpForce = 5;
-
-    private float _coyoteTime = 0.2f;
-    private float _coyoteTimeCounter;
-
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class BasicPlayerMovement : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody2D>();
-    }
+        private Rigidbody2D _rb;
+        private float _xInput;
+        [SerializeField] private float speed = 5;
 
-    private void Update()
-    {
-        _xInput = Input.GetAxis("Horizontal");
+        private bool _performJump;
+        private bool _isGrounded;
+        [SerializeField] private float jumpForce = 5;
 
-        if (_isGrounded)
-        {
-            _coyoteTimeCounter = _coyoteTime;
-        }
-        else
-        {
-            _coyoteTimeCounter -= Time.deltaTime;
-        }
+        [SerializeField] private float coyoteTime = 0.4f;
+        private float _coyoteTimeCounter;
 
-        if (Input.GetButtonDown("Jump") && _coyoteTimeCounter > 0)
+        private void Awake()
         {
-            _performJump = true;
+            _rb = GetComponent<Rigidbody2D>();
         }
 
-        if (Input.GetButtonUp("Jump"))
+        private void Update()
         {
-            _coyoteTimeCounter = 0;
+            _xInput = Input.GetAxis("Horizontal");
+
+            if (_isGrounded)
+            {
+                _coyoteTimeCounter = coyoteTime;
+            }
+            else
+            {
+                _coyoteTimeCounter -= Time.deltaTime;
+            }
+
+            if (Input.GetButtonDown("Jump") && _coyoteTimeCounter > 0)
+            {
+                _performJump = true;
+            }
+
+            if (Input.GetButtonUp("Jump"))
+            {
+                _coyoteTimeCounter = 0;
+            }
         }
-    }
 
-    private void FixedUpdate()
-    {
-        _rb.linearVelocity = new Vector2(_xInput * speed, _rb.linearVelocity.y);
-
-        if (_performJump)
+        private void FixedUpdate()
         {
-            _performJump = false;
+            _rb.linearVelocity = new Vector2(_xInput * speed, _rb.linearVelocity.y);
+
+            if (_performJump)
+            {
+                _performJump = false;
+                _isGrounded = false;
+                _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Vector3 normal = collision.GetContact(0).normal;
+            if (normal.y > 0.5f)
+            {
+                _isGrounded = true;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
             _isGrounded = false;
-            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        _isGrounded = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        _isGrounded = false;
     }
 }
