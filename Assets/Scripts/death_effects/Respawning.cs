fcef800ef2f729c5947ee4b50eb89d8b9826026b
Tomasz Nehring <tomasz_nehring@outlook.com>
2025-10-13 10:27:42 +0200

    test(audio): respawn sound was attached to the experimental hero model
    chore(unity): unity has preprocessed new audio sounds creating *.meta files

--- Assets/Scripts/death_effects/Respawning.cs
+++ Assets/Scripts/death_effects/Respawning.cs
@@ -1,17 +1,26 @@
-﻿using death_effects.interfaces;
+﻿using System;
+using death_effects.interfaces;
 using death_processors;
 using UnityEngine;
 
 namespace death_effects
 {
-    [RequireComponent(typeof(Terminable))]
+    [RequireComponent(typeof(Terminable), typeof(AudioSource))]
     public class Respawning : MonoBehaviour, ITerminationReplacement
     {
         // Fields
         [SerializeField] private Transform respawnTransform;
+        [SerializeField] private AudioClip respawnSound;
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            this._audioSource = this.GetComponent<AudioSource>();
+        }
 
         public void Terminate()
         {
+            this._audioSource.PlayOneShot(this.respawnSound);
             this.transform.position = this.respawnTransform.position;
         }
         
