43489d1277d8d66d4abd13dfe0a88cf817fbf260
Tomasz Nehring <tomasz_nehring@outlook.com>
2025-10-13 10:55:20 +0200

    feat(aggregator-components): added 2 aggregator components, which merely pull/require other components to/for entity

---
+++ Assets/Scripts/aggregators/HeroLifeCycle.cs
@@ -0,0 +1,10 @@
+using death_effects;
+using UnityEngine;
+
+namespace aggregators
+{
+    [RequireComponent(typeof(SceneBound), typeof(Respawning))]
+    public class HeroLifeCycle : MonoBehaviour
+    {
+    }
+}
