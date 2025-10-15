d53d8e5085c20ccd7660de077469f380aa68aa54
Tomasz Nehring <tomasz_nehring@outlook.com>
2025-10-15 20:22:09 +0200

    refactor(magic_number): removed magic number from Assets/Scripts/effects/PersistentPoison.cs

--- Assets/Scripts/effects/PersistentPoison.cs
+++ Assets/Scripts/effects/PersistentPoison.cs
@@ -11,13 +11,17 @@ namespace effects
         [SerializeField] private uint damagePerTick = 1;
         [SerializeField] private float tickInterval = 2.5f;
         [SerializeField] private AudioClip poisoningSound;
-
-        private float _timeSinceLastTick = 0f;
+        // Constants
+        private float _timeBeforeFirstTick;
         private Hurtable _hurtable;
         private Mortal _mortal;
+        // Variables
+        private float _timeTillNextTick;
 
         private void Awake()
         {
+            _timeBeforeFirstTick = tickInterval / 2;
+            _timeTillNextTick = _timeBeforeFirstTick;
             _hurtable = GetComponent<Hurtable>();
             _mortal = GetComponent<Mortal>();
         }
@@ -26,22 +30,22 @@ namespace effects
         {
             if (_mortal.IsAlive)
             {
-                _timeSinceLastTick += Time.deltaTime;
-                if (_timeSinceLastTick >= tickInterval)
+                _timeTillNextTick -= Time.deltaTime;
+                if (_timeTillNextTick <= 0)
                 {
+                    _timeTillNextTick += tickInterval;
                     Tick();
                 }
             }
             else
             {
-                _timeSinceLastTick = tickInterval/2; // Reset timer to half of interval when dead
+                _timeTillNextTick = _timeBeforeFirstTick;
             }
         }
 
         private void Tick()
         {
             _hurtable.ReceiveDamage(damagePerTick, poisoningSound);
-            _timeSinceLastTick = 0f;
         }
     }
 }
