efd1560e2c1bda15fba7cd47b48c1f8a81189041
Filip <filip@jelski.net>
2025-10-27 04:45:06 +0100

    feat(UI coin counter added)

--- Assets/Scripts/collectables/CollectableCoinSO.cs
+++ Assets/Scripts/collectables/CollectableCoinSO.cs
@@ -1,12 +1,18 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CollectableCoinSO", menuName = "Scriptable Objects/CollectableCoinSO")]
 public class CollectableCoinSO : CollectableSOBase
 {
+    
     [Header("Stats")] public int value = 1;
     [Header("Content")] public AudioClip audioClip;
+    
+    public static event Action<int> OnCoinCollected;
+    
     public override void Collect(GameObject collector)
     {
         collector.GetComponent<AudioSource>().PlayOneShot(audioClip);
+        OnCoinCollected?.Invoke(value);
     }
 }
---
+++ Assets/Scripts/collectables/CurrencyManager.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text myText;
+    private int coins = 0;
+    
+    void Start()
+    {
+        myText.text = 0.ToString();
+    }
+    
+    void OnEnable()
+    {
+        CollectableCoinSO.OnCoinCollected += AddCoins;
+    }
+    
+    private void AddCoins(int value)
+    {
+        coins += value;
+        myText.text = coins.ToString();
+    }
+    
+    
+}
