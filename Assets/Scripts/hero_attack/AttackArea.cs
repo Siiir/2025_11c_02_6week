ece39b2fac5e06f2de693b62bca4a913f0b89187
Tomasz Nehring <tomasz_nehring@outlook.com>
2025-10-27 11:40:20 +0100

    chore(formatting): optimized imports, formatted

--- Assets/Scripts/hero_attack/AttackArea.cs
+++ Assets/Scripts/hero_attack/AttackArea.cs
@@ -3,23 +3,21 @@ using UnityEngine;
 
 public class AttackArea : MonoBehaviour
 {
-    
     private uint damage;
 
     public void SetDamage(uint dmg)
     {
         damage = dmg;
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) return;
         Hurtable target = collision.GetComponent<Hurtable>();
-        
+
         if (target != null)
         {
             target.ReceiveDamage(damage);
         }
-        
     }
 }
