089a8cad2cf0a0dfb2bd03015ebea45da96f6916
Tomasz Nehring <tomasz_nehring@outlook.com>
2025-11-10 14:07:36 +0100

    fix(fmt): formatting and import cleanup

--- Assets/Scripts/death_effects/DestroyAfterTime.cs
+++ Assets/Scripts/death_effects/DestroyAfterTime.cs
@@ -5,6 +5,7 @@ namespace death_effects
     public class DestroyAfterTime : MonoBehaviour
     {
         [SerializeField] private float lifeTimeLeft = 3f;
+
         void Start()
         {
             Destroy(gameObject, lifeTimeLeft);
