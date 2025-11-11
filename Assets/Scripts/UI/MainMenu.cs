37e2591b3b8d7241d4fcab41aaf89f3efa7474ec
Filipustyyy <filip@jelski.net>
2025-11-11 18:06:47 +0100

    feat(MainMenu script change)

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,16 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // MAKE SURE it's whatever number is the level1Scene in build settings
-    private readonly int _level1Scene = 1;
     public void PlayGame()
     {
-        SceneManager.LoadScene(_level1Scene);
+        SceneManager.LoadScene("Level1Scene");
     }
     
     public void ExitGame()
     {
-        
         Application.Quit();
         
 #if UNITY_EDITOR
