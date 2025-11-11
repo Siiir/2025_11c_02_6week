52df8030c8b468d4378b051d6578cc91c1e23eef
Filipustyyy <filip@jelski.net>
2025-11-11 06:56:56 +0100

    feat(buttons)

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // MAKE SURE ITS WHATEVER NUMBER IS THE LEVEL 1 SCENE IN BUILD SETTINGS
+    private int _level1Scene = 1;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(_level1Scene);
+    }
+    
     public void ExitGame()
     {
         Debug.Log("Exit button pressed!");
